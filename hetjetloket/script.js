67b35dff30e9b311b9afea3ebf82b0671e5f6d3b
mikedegrassi <89838846+mikedegrassi@users.noreply.github.com>
2022-05-17 14:44:50 +0200

    Add files via upload

---
+++ hetjetloket/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+      if($(document).scrollTop()>50){
+        $("nav").addClass("shrink")
+         } else{
+           $("nav").removeClass("shrink")
+         }
+    });
+  });
+  
+  
